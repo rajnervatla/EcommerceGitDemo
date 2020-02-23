6ef14c433fa0ff2cfbf74dc64500a27eaf4546a2
rnervatl <rnervatl@LIN35007796.corp.capgemini.com>
2020-02-23 12:37:32 +0530

    Commiting resi

---
+++ src/main/java/com/qa/Ecommerce/ResiEmail.java
@@ -0,0 +1,12 @@
+package com.qa.Ecommerce;
+
+public class ResiEmail {
+public static void main(String[] args) {
+	System.out.println("Adding the new class on 19th Feb");
+	for(int i=0;i<15;i++)
+	{
+		System.out.println(i);
+	}
+	
+}
+}
--- src/main/java/com/qa/Ecommerce/Testing_git.java
+++ src/main/java/com/qa/Ecommerce/Testing_git.java
@@ -4,5 +4,6 @@ public class Testing_git {
 public static void main(String[] args) {
 	String name="Rajkumar Nervatla";
 	System.out.println("Name of the Employee:: "+name);
+	System.out.println("Code change on 19th feb");
 }
 }
