8ab172f7fc16fa23ec4219d2ac73bf86afde9503
rnervatl <rnervatl@LIN35007796.corp.capgemini.com>
2020-02-23 16:42:37 +0530

    pushing the new class for testing

---
+++ src/main/java/com/qa/Ecommerce/FW.java
@@ -0,0 +1,7 @@
+package com.qa.Ecommerce;
+
+public class FW {
+public static void main(String[] args) {
+	System.out.println("Main Testing the Application ...");
+}
+}
---
+++ src/main/java/com/qa/Ecommerce/PrintClass.java
@@ -0,0 +1,9 @@
+package com.qa.Ecommerce;
+
+public class PrintClass {
+public static void main(String[] args) {
+	 int var=10;
+	System.out.println(var);
+	System.out.println("");
+}
+}
